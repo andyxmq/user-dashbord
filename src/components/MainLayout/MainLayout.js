922206916ff8e88202d371a9c854548673715a4b
徐明权 <xumingquan@xumingquandeMacBook-Pro.local>
2017-11-08 00:19:52 +0800

    models/users.js

---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MainLayout extends React.Component {
+    render() {
+        return '';
+    }
+}
+
+export default MainLayout;
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Button } from 'antd';
 import styles from './Users.css';
 
 function Users() {
   return (
     <div className={styles.normal}>
       Route Component: Users
+      <Button>aaa</Button>
     </div>
   );
 }
