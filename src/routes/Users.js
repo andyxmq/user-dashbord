41d1ba4abe2c24d37e14df8e7e6217f9b522f82c
徐明权 <xumingquan@xumingquandeMacBook-Pro.local>
2017-11-13 00:05:17 +0800

    aa

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Button } from 'antd';
+import { Table, Popconfirm } from 'antd';
 import styles from './Users.css';
 
-function Users() {
-    return (
-      <div className={styles.normal}>
-          Route Component: Users
-          <Button>aaa</Button>
-      </div>
-    );
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    deleteHandler(id) {
+        console.warn(`TODO: ${id}`);
+    }
+    render() {
+        const columns = [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+            }, {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email',
+            }, {
+                title: 'Website',
+                dataIndex: 'website',
+                key: 'website',
+            }, {
+                title: 'Operation',
+                key: 'operation',
+                render: (text, { id }) => (
+                  <span className={styles.operation}>
+                    <a href="">Edit</a>
+                    <Popconfirm title="Confirm to Delete" onConfirm={() => this.deleteHandler(null, id)}>
+                      <a href="">Delete</a>
+                    </Popconfirm>
+                  </span>
+                ),
+            },
+        ];
+
+        const dataSource = [
+          { name: '111', email: '1714903660@qq.com', website: 'www.baidu.com' },
+        ];
+        return (
+          <div className={styles.normal}>
+            <div>
+              <Table
+                columns={columns}
+                dataSource={dataSource}
+              />
+
+            </div>
+          </div>
+        );
+    }
 }
 
 function mapStateToProps() {
